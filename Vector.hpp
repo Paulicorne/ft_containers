494b08296ceba1cc659f1ca80eff238a3ed4d4e0
Pauline Millet <milletp.pro@gmail.com>
2022-06-14 15:53:55 +0200

    learning vector class heritage tree

---
+++ Vector.hpp
@@ -0,0 +1,31 @@
+#include <memory> // allocators
+
+namespace ft
+{
+	template <bool>
+	class __vector_base_common
+	{
+		protected :
+				__vector_base_common() {} // add visibility tags ?
+				void	__throw_length_error() const;
+				void	__throw_out_of_range() const;
+	};
+
+	
+
+	// __vector_base
+
+	template <class _Tp, class _Allocator>
+	class __vector_base : protected __vector_base_common<true>
+	{
+
+	}
+	
+	template <class _Tp, class _Alloc = std::allocator<_Tp> >
+	class vector : private __vector_base<_Tp, _Allocator> // add visibility thingy ?
+	{
+
+	}
+
+
+}
